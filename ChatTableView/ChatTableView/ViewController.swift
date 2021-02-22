0c64df24e2d8c1498c0ea4b42542c613bf425d08
TonyJem <a.jemanov@gmail.com>
2021-02-22 21:03:02 +0200

    Add custom cell to viewcontroller's tableview

--- ChatTableView/ChatTableView/ViewController.swift
+++ ChatTableView/ChatTableView/ViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var mainTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nib = UINib(nibName: "RegularCell", bundle: nil)
+        mainTable.register(nib, forCellReuseIdentifier: "RegularCell")
+        mainTable.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RegularCell") as! RegularCell
+        return cell
+    }
 }
